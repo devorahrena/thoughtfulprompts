37c12a2a4854309882cbbba2187d805e39f05295
Devorah Simon <devorahrsimon@gmail.com>
2025-06-20 16:59:24 -0400

    fix src again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thoughtfulprompts/', // ← this must match your GitHub repo name
+  base: '/thoughtfulprompts/',
   plugins: [react()],
 })
